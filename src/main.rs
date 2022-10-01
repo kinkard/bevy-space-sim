d4201fa3ace9629e3d4b98c67d1311ce4b139f55
Kinkard <stsiapan.kizim@mapbox.com>
2022-10-02 00:22:24 +0300

    Add wireframe and change mouse orientation logic

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
-use bevy::{input::mouse::MouseMotion, prelude::*};
+use bevy::pbr::wireframe::{WireframeConfig, WireframePlugin};
+use bevy::prelude::*;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
+        .add_plugin(WireframePlugin)
         .add_startup_system(setup)
         .add_system(move_camera)
         .add_system(bevy::window::close_on_esc)
@@ -13,7 +15,34 @@ fn setup(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
+    asset_server: Res<AssetServer>,
+    mut wireframe_config: ResMut<WireframeConfig>,
 ) {
+    wireframe_config.global = true;
+
+    // root UI node that covers all screen
+    commands
+        .spawn_bundle(NodeBundle {
+            style: Style {
+                size: Size::new(Val::Percent(100.0), Val::Percent(100.0)),
+                align_items: AlignItems::Center, // vertical alignment
+                justify_content: JustifyContent::Center, // horizontal alignment
+                ..default()
+            },
+            color: Color::NONE.into(),
+            ..default()
+        })
+        .with_children(|parent| {
+            parent.spawn_bundle(ImageBundle {
+                style: Style {
+                    size: Size::new(Val::Px(40.0), Val::Px(40.0)),
+                    ..default()
+                },
+                image: asset_server.load("UI/aim.png").into(),
+                ..default()
+            });
+        });
+
     // Create a sky
     commands.spawn_bundle(PbrBundle {
         mesh: meshes.add(Mesh::from(shape::Capsule {
@@ -37,7 +66,7 @@ fn setup(
     //Create a ground
     commands
         .spawn_bundle(PbrBundle {
-            mesh: meshes.add(Mesh::from(shape::Plane { size: 15.0 })),
+            mesh: meshes.add(Mesh::from(shape::Plane { size: 150.0 })),
             material: materials.add(Color::rgb(0.3, 0.5, 0.3).into()),
             ..default()
         })
@@ -45,7 +74,14 @@ fn setup(
 
     // Create a light
     commands.spawn_bundle(PointLightBundle {
-        transform: Transform::from_xyz(4.0, 8.0, 4.0),
+        point_light: PointLight {
+            intensity: 4000.0,
+            range: 100.0,
+            radius: 2.0,
+            shadows_enabled: true,
+            ..default()
+        },
+        transform: Transform::from_xyz(4.0, 10.0, 4.0),
         ..default()
     });
 
@@ -59,19 +95,18 @@ fn setup(
 fn move_camera(
     time: Res<Time>,
     keys: Res<Input<KeyCode>>,
-    mut mouse_ev: EventReader<MouseMotion>,
+    mouse: Res<Input<MouseButton>>,
     mut windows: ResMut<Windows>,
+    mut mouse_guidance: Local<bool>,
     mut query: Query<&mut Transform, With<Camera3d>>,
 ) {
     let mut camera_speed = 10.0;
     if keys.pressed(KeyCode::LShift) {
-        camera_speed *= 2.0;
+        camera_speed *= 3.0;
     }
     let camepa_step = camera_speed * time.delta_seconds();
 
     let mut translation = Vec3::ZERO;
-    let mut rotation = Quat::IDENTITY;
-
     if keys.pressed(KeyCode::W) {
         // strafe up
         translation.y += camepa_step;
@@ -96,6 +131,8 @@ fn move_camera(
         // move backward
         translation.z += camepa_step;
     }
+
+    let mut rotation = Quat::IDENTITY;
     if keys.pressed(KeyCode::Q) {
         // rotate counter clockwise
         rotation *= Quat::from_rotation_z(camepa_step * 10.0_f32.to_radians());
@@ -105,18 +142,31 @@ fn move_camera(
         rotation *= Quat::from_rotation_z(camepa_step * -10.0_f32.to_radians());
     }
 
-    // toggle cursor
+    // Enable mouse guidance if Space is pressed
     let window = windows.primary_mut();
     if keys.just_released(KeyCode::Space) {
-        window.set_cursor_lock_mode(!window.cursor_locked());
-        window.set_cursor_visibility(!window.cursor_visible());
+        *mouse_guidance = !*mouse_guidance;
+        let icon = if *mouse_guidance {
+            CursorIcon::Crosshair
+        } else {
+            CursorIcon::Default
+        };
+        window.set_cursor_icon(icon);
     }
 
-    // Use mouse rotation if cursort is locked
-    if window.cursor_locked() {
-        for ev in mouse_ev.iter() {
-            rotation *= Quat::from_rotation_y(0.1 * ev.delta.x.to_radians());
-            rotation *= Quat::from_rotation_x(0.1 * ev.delta.y.to_radians());
+    if *mouse_guidance || mouse.pressed(MouseButton::Left) {
+        let center = Vec2 {
+            x: window.width() / 2.0,
+            y: window.height() / 2.0,
+        };
+
+        if let Some(pos) = window.cursor_position() {
+            let offset = center - pos;
+            // Safe zone around screen center
+            if offset.length_squared() > 400.0 {
+                rotation *= Quat::from_rotation_y(0.005 * offset.x.to_radians());
+                rotation *= Quat::from_rotation_x(-0.005 * offset.y.to_radians());
+            }
         }
     }
 
